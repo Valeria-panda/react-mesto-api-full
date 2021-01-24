1f2ba0248441b9ea7924966e9c074213f82769a1
Valeriya Zolotaryova <Panda.lera@mail.ru>
2021-01-25 00:36:21 +0500

    reorganized structure

---
+++ frontend/src/utils/auth.js
@@ -0,0 +1,71 @@
+import BadRequestError from '../errors/badRequestError';
+import NotAuthorizedError from '../errors/notAuthorizedError';
+// старый вариант
+// export const BASE_URL = 'https://auth.nomoreparties.co';
+
+export const BASE_URL = 'https://www.lera.students.nomoredomains.rocks';
+
+// Отправляем запрос на регистрацию
+export const register = (password, email) => fetch(`${BASE_URL}/signup`, {
+  method: 'POST',
+  headers: {
+    'Content-Type': 'application/json',
+  },
+  body: JSON.stringify({ password, email }),
+})
+  .then((res) => {
+    if (!res.ok) {
+      return res.json()
+        .then((err) => {
+          if (err.error) {
+            throw new BadRequestError(err.error);
+          } else {
+            throw new BadRequestError(err.message);
+          }
+        });
+    }
+    return res.json();
+  });
+
+// Отправляем запрос на авторизацию
+export const authorize = (password, email) => fetch(`${BASE_URL}/signin`, {
+  method: 'POST',
+  headers: {
+    'Content-Type': 'application/json',
+  },
+  body: JSON.stringify({ password, email }),
+})
+  .then((res) => {
+    if (res.status === 400) {
+      throw new BadRequestError('Не передано одно из полей');
+    }
+    else if (res.status === 401) {
+      throw new NotAuthorizedError('Пользователь с таким email не найден');
+    }
+    return res.json();
+  })
+  .then((data) => {
+    if (data.token) {
+      localStorage.setItem('jwt', data.token);
+      return data.token;
+    }
+  });
+
+// Отправляем запрос на получение токена
+export const getContent = (token) => fetch(`${BASE_URL}/users/me`, {
+  method: 'GET',
+  headers: {
+    'Content-Type': 'application/json',
+    'Authorization': `Bearer ${token}`,
+  },
+})
+  .then((res) => {
+    if (!res.ok) {
+      return res.json()
+        .then((err) => {
+          throw new NotAuthorizedError(err.message);
+        });
+    }
+    return res.json()
+  })
+  .then((data) => data);
