dcd5a13afc09371410c9bc60031fefe6842f26a3
Valeriya Zolotaryova <Panda.lera@mail.ru>
2021-01-25 23:35:39 +0500

    upd

--- backend/controllers/users.js
+++ backend/controllers/users.js
@@ -70,7 +70,7 @@ module.exports.login = (req, res, next) => {
           httpOnly: true,
           sameSite: true,
         })
-        .send({ message: 'Успешная авторизация' });
+        .send(user);
     })
     .catch(next);
 };
--- frontend/src/utils/auth.js
+++ frontend/src/utils/auth.js
@@ -5,13 +5,15 @@ import NotAuthorizedError from '../errors/notAuthorizedError';
 
 export const BASE_URL = 'https://api.advent.students.nomoredomains.rocks';
 
-// Отправляем запрос на регистрацию
-// export const register = (password, email, avatar) => fetch(`${BASE_URL}/signup`, {
+
+
+// Отправляем запрос за регистрацию
+// export const register = (password, email) => fetch(`${BASE_URL}/signup`, {
 //   method: 'POST',
 //   headers: {
 //     'Content-Type': 'application/json',
 //   },
-//   body: JSON.stringify({ password, email, avatar }),
+//   body: JSON.stringify({ password, email }),
 // })
 //   .then((res) => {
 //     if (!res.ok) {
@@ -25,9 +27,9 @@ export const BASE_URL = 'https://api.advent.students.nomoredomains.rocks';
 //         });
 //     }
 //     return res.json();
-// });
+//   });
 
-// // Отправляем запрос на авторизацию
+// // Отправляем запрос за авторизацию
 // export const authorize = (password, email) => fetch(`${BASE_URL}/signin`, {
 //   method: 'POST',
 //   headers: {
@@ -51,9 +53,7 @@ export const BASE_URL = 'https://api.advent.students.nomoredomains.rocks';
 //     }
 //   });
 
-
-
-// // Отправляем запрос на получение токена
+// // Отправляем запрос за получение токена
 // export const getContent = (token) => fetch(`${BASE_URL}/users/me`, {
 //   method: 'GET',
 //   headers: {
@@ -72,72 +72,70 @@ export const BASE_URL = 'https://api.advent.students.nomoredomains.rocks';
 //   })
 //   .then((data) => data);
 
-
-
-
-
-
-// Отправляем запрос за регистрацию
-export const register = (password, email) => fetch(`${BASE_URL}/signup`, {
-  method: 'POST',
-  headers: {
-    'Content-Type': 'application/json',
-  },
-  body: JSON.stringify({ password, email }),
-})
-  .then((res) => {
-    if (!res.ok) {
-      return res.json()
-        .then((err) => {
-          if (err.error) {
-            throw new BadRequestError(err.error);
-          } else {
-            throw new BadRequestError(err.message);
+  export const register = (password, email) => {
+    return fetch(`${BASE_URL}/signup`, {
+      method: 'POST',
+      headers: {
+        'Accept': 'application/json',
+        'Content-Type': 'application/json'
+      },
+  
+      body: JSON.stringify({ password, email })
+    })
+      .then((res) => {
+          if (!res.ok) {
+            return res.json()
+              .then((err) => {
+                if (err.error) {
+                  throw new BadRequestError(err.error);
+                } else {
+                  throw new BadRequestError(err.message);
+                }
+              });
           }
-        });
-    }
-    return res.json();
-  });
-
-// Отправляем запрос за авторизацию
-export const authorize = (password, email) => fetch(`${BASE_URL}/signin`, {
-  method: 'POST',
-  headers: {
-    'Content-Type': 'application/json',
-  },
-  body: JSON.stringify({ password, email }),
-})
-  .then((res) => {
-    if (res.status === 400) {
-      throw new BadRequestError('Не передано одно из полей');
-    }
-    else if (res.status === 401) {
-      throw new NotAuthorizedError('Пользователь с таким email не найден');
-    }
-    return res.json();
-  })
-  .then((data) => {
-    if (data.token) {
-      localStorage.setItem('jwt', data.token);
-      return data.token;
-    }
-  });
-
-// Отправляем запрос за получение токена
-export const getContent = (token) => fetch(`${BASE_URL}/users/me`, {
-  method: 'GET',
-  headers: {
-    'Content-Type': 'application/json',
-    'Authorization': `Bearer ${token}`,
-  },
-})
-  .then((res) => {
-    if (!res.ok) {
-      return res.json()
-        .then((err) => {
-          throw new NotAuthorizedError(err.message);
-        });
-    }
-    return res.json()
-  })
-  .then((data) => data);
+          return res.json();
+    });
+  }
+  
+  export const authorize = (password, email) => {
+    return fetch(`${BASE_URL}/signin`, {
+      method: 'POST',
+      headers: {
+        Accept: 'application/json',
+        'Content-Type': 'application/json',
+       // authorization: `Bearer ${localStorage.getItem('token')}`
+      },
+      body: JSON.stringify({ password, email })
+    })
+      .then((res => {
+        if (res.status === 400) {
+          throw new BadRequestError('Не передано одно из полей');
+        }
+        if (res.status === 401) {
+          throw new NotAuthorizedError('Пользователь с таким email не найден');
+        }
+        return res.json();
+      })
+      )
+      .then((data) => {
+        if (data.token) {
+          localStorage.setItem('token', data.token);
+          return data;
+        } else {
+          return;
+        }
+      })
+  };
+  
+  export const getContent = (token) => {
+    return fetch(`${BASE_URL}/users/me`, {
+      method: 'GET',
+      headers: {
+        Accept: 'application/json',
+        'Content-Type': 'application/json',
+        'Authorization': `Bearer ${token}`,
+      }
+    })
+      .then(res => res.json())
+     // .then(data => data)
+  }
