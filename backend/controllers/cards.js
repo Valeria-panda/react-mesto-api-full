edfec99f75a281ae99cae76db02536a50c86db8b
Valeriya Zolotaryova <Panda.lera@mail.ru>
2021-01-29 23:15:14 +0500

    upd

--- backend/controllers/cards.js
+++ backend/controllers/cards.js
@@ -11,16 +11,30 @@ module.exports.getCards = (req, res) => {
     .catch((err) => res.status(500).send({ message: `На сервере произошла ошибка: ${err.message}` }));
 };
 
+// module.exports.createCard = (req, res, next) => {
+//   module.exports.createCard = (req, res) => Card.create({
+//     name: req.body.name,
+//     link: req.body.link,
+//     owner: req.user._id
+//   })
+//     .catch((err) => {
+//       throw new BadRequestError({ message: `Указаны некорректные данные при создании карточки: ${err.message}` });
+//     })
+//     .then((card) => res.send(card))
+//     .catch(next);
+// };
+
 module.exports.createCard = (req, res, next) => {
-  module.exports.createCard = (req, res) => Card.create({
-    name: req.body.name,
-    link: req.body.link,
-    owner: req.user._id
-  })
-    .catch((err) => {
-      throw new BadRequestError({ message: `Указаны некорректные данные при создании карточки: ${err.message}` });
+  const { name, link } = req.body;
+
+  Card.create({ name, link, owner: req.user._id })
+    .then((card) => {
+      if (!card) {
+        throw new BadRequestError('Переданы некорректные данные');
+      }
+
+      return res.send(card);
     })
-    .then((card) => res.send(card))
     .catch(next);
 };
 
